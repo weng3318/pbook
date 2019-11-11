7a7fba7fe5386a1120727ed96cc16a042af483f6
Wei Hsun <drunkhsun@gmail.com>
2019-11-11 17:26:28 +0800

    1111weihsun

--- pbook-team/src/components/shop/ShopReducers.js
+++ pbook-team/src/components/shop/ShopReducers.js
@@ -1,49 +1,45 @@
-import { combineReducers } from 'redux'
 import { CATEGORIES_RECEIVE, CATEGORIES_REQUEST } from './ShopActions'
 
 function cg(
   state = {
     isFetching: false,
     didInvalidate: false,
-    cgName: [],
-    id: [],
   },
   action
 ) {
   switch (action.type) {
+    case CATEGORIES_RECEIVE:
+      return {
+        ...state,
+        isFetching: false,
+        didInvalidate: false,
+        payload: action.payload,
+        lastUpdated: action.receivedAt,
+      }
     case CATEGORIES_REQUEST:
       return {
         ...state,
         isFetching: true,
         didInvalidate: false,
       }
-    case CATEGORIES_RECEIVE:
-      return {
-        ...state,
-        isFetching: false,
-        didInvalidate: false,
-        cgName: action.cgData.name,
-        id: action.cgData.sid,
-        lastUpdated: action.receivedAt,
-      }
     default:
       return state
   }
 }
 
-function cgData(state = [], action) {
+function categories(state = [], action) {
   switch (action.type) {
     case CATEGORIES_RECEIVE:
     case CATEGORIES_REQUEST:
       return {
         ...state,
-        [action.payload]: cg(state[action.payload], action),
+        ...cg(state[action], action),
       }
     default:
       return state
   }
 }
 
-const ShopReducers = { cgData }
+const ShopReducers = { categories }
 
 export default ShopReducers
