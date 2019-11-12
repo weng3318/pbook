232e8fd570789ca34faa64a7632dc3cfb6d72b9b
luke <weu.che0812@gmail.com>
2019-11-12 11:29:32 +0800

    調整localstorage出來的欄位

--- pbook-team/src/pages/member/Edit.js
+++ pbook-team/src/pages/member/Edit.js
@@ -11,47 +11,24 @@ const Edit = () => {
             <div className="list">
               <div className="d-flex item">
                 <h4>帳號 : </h4>
-                <input type="text" />
+                <input type="text" id="email" name="email" />
               </div>
               <div className="d-flex item">
                 <h4>姓名 : </h4>
-                <input type="text" />
+                <input type="text" id="name" name="name" />
               </div>
               <div className="d-flex item">
                 <h4>暱稱 : </h4>
-                <input type="text" />
+                <input type="text" id="nickname" name="nickname" />
               </div>
               <div className="d-flex item">
-                <h4>信箱 : </h4>
-                <input type="text" />
+                <h4>生日 : </h4>
+                <input type="text" id="birthday" name="birthday" />
               </div>
-              <div className="d-flex item">
-                <h4>性別 : </h4>
-                <div className="d-flex genderGroup">
-                  <input
-                    className=""
-                    type="radio"
-                    name="gender"
-                    value="1"
-                  />
-                  <h4>
-                    男
-                  </h4>
-                  <input
-                    className=""
-                    type="radio"
-                    name="gender"
-                    value="2"
-                  />
-                  <h4 className="" for="gender">
-                    女
-                  </h4>
-                </div>
-                
-              </div>
+
               <div className="d-flex item">
                 <h4>手機 : </h4>
-                <input type="text" />
+                <input type="text" id="mobile" name="mobile" />
               </div>
               <div className="d-flex item">
                 <h4>地址 : </h4>
@@ -65,25 +42,49 @@ const Edit = () => {
               </div>
             </div>
             <div className="list-r">
-              <div className="item">
+              <div className="itemTitle">
                 <h3>會員編號 :</h3>
                 <h3>MR000001</h3>
               </div>
-              <div className="item">
+              <div className="itemTitle">
                 <h3>會員等級 :</h3>
                 <h3>品書大師</h3>
               </div>
-              <div className="item column">
+              <div className="item">
                 <figure>
                   <img src="../images/cars.jpg" alt="" />
                 </figure>
-                <button>更換</button>
+                <div className="chang_btn">
+                <input type="file" class="form-control-file" id="mr_pic" name="mr_pic" style={{display:"none"}} />
+                
+                <button className="btn btn-warning my-2 my-sm-0" type="button" onclick="uploadFile()">
+                                選擇圖片
+                </button>
+                </div>
               </div>
             </div>
           </div>
           <div className="d-flex button-group">
-            <button>取消</button>
-            <button>確認</button>
+            <div>
+              <button
+                style={{ width: '130px' }}
+                type="submit"
+                class="btn btn-warning"
+                id="submit_btn"
+              >
+                &nbsp;取&nbsp;&nbsp;&nbsp;消&nbsp;
+              </button>
+            </div>
+            <div>
+              <button
+                style={{ width: '130px' }}
+                type="submit"
+                class="btn btn-warning"
+                id="submit_btn"
+              >
+                &nbsp;確&nbsp;認&nbsp;修&nbsp;改&nbsp;
+              </button>
+            </div>
           </div>
         </div>
       </div>
