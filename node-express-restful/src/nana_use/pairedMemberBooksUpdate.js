7cd76140313f044a0bd29a19bf66a727a548307f
nana81210 <nana81210@hotmail.com>
2019-11-18 18:25:40 +0800

    2019/11/18 18:25 nana上傳

--- node-express-restful/src/nana_use/pairedMemberBooksUpdate.js
+++ node-express-restful/src/nana_use/pairedMemberBooksUpdate.js
@@ -1,6 +1,6 @@
 // 引入套件
 const express = require("express"); //EXPRESS(建立路由使用)
-const countDown = express.Router();
+const pairedMemberBooksUpdate = express.Router();
 const bluebird = require("bluebird"); //青鳥
 const _ = require("lodash"); //loadsh,處理數據的各種方法
 
@@ -18,20 +18,24 @@ db.connect(); //資料庫連線
 bluebird.promisifyAll(db);
 
 
-countDown
-    .route("/countDown")
+pairedMemberBooksUpdate
+    .route("/pairedMemberBooksUpdate")
     .post(function (req, res) {
         console.log('pairedMemberBooksUpdate', req.body.memberId);
+        console.log('pairedMemberBooksUpdate', req.body.pairedMemberBooks);
+        console.log('pairedMemberBooksUpdate', req.body.startTime);
+        console.log('pairedMemberBooksUpdate', req.body.GameChance);
+        var pairedMemberBooks = JSON.stringify(req.body.pairedMemberBooks)
         db.queryAsync(
-            `SELECT GameCreatedTime FROM mb_gamelist WHERE MR_number = "${req.body.memberId}"`
+            `UPDATE mb_gamelist SET GamePairedMemberBooks ='${pairedMemberBooks}',GameCreatedTime='${req.body.startTime}',GameChance='${req.body.GameChance}' WHERE MR_number = '${req.body.memberId}'`
         )
             .then(results => {
-                res.json(results);
+                res.send('資料庫已更新成新資料');
             })
             .catch(error => {
                 res.send("404-找不到資料");
                 console.log(error);
             });
-    });
-module.exports = countDown;
 
+    });
+module.exports = pairedMemberBooksUpdate;
