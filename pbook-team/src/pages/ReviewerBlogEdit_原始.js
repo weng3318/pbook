02b87a2373b735c7bf7b9d3d770037c7ffa6d560
weng3318 <weng7395@gmail.com>
2019-11-25 19:25:23 +0800

    1125t0725 CKEdit編輯

--- pbook-team/src/pages/ReviewerBlogEdit_原始.js
+++ pbook-team/src/pages/ReviewerBlogEdit_原始.js
@@ -7,58 +7,25 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 export class ReviewerBlogEdit extends React.Component {
   constructor(props) {
     super(props)
-    this.elementName = "editor_" + props.id;
     this.state = {
       csData: [],
-      init: false,
     }
   }
-  componentDidMount(){
-        let newcsData
-        axios
-          .get('http://localhost:5555/reviewer/brBookcase')
-          .then(res => {
-            newcsData = res.data.rows
-            return axios.get('http://localhost:5555/reviewer/brbooks')
-          })
-          .then(res => {
-            this.setState({ csData: newcsData, bkData: res.data.rows })
-          })
-          .catch(function(error) {
-            console.log('前端沒有取得資料', error)
-          })
-
-    // 當該組件用於創建模塊時，需要渲染editor (條件：之前從沒初始化過)
-          if (this.props.content === '' && this.props.id && this.state.init === false) {
-            this.loadCkEditor(this.props);
-          }
-        }
-    // 每次更新都會走進該週期，需要判斷有沒有初始化過 && 外部有傳進內容
-    // 當沒有init === false的判斷，會導致頁面渲染多個編輯器，
-    // 開始的解決方案不是用init作為標記，而是使用ckeditor的destory、delete等方法，發現實現起來都不太乾淨，且有問題
-        componentWillReceiveProps(nextProps) {
-    
-          if (CKEditor && nextProps && nextProps.content && nextProps.id  && this.state.init === false) {
-            this.loadCkEditor(nextProps);
-          }
-        }
-    
-        loadCkEditor = (nextProps) => {
-          this.elementName = "editor_" + this.props.id;
-    
-          CKEditor.replace(this.elementName);
-          const { onChange } = nextProps;
-    
-          CKEditor.instances[this.elementName].on("change", function () {
-            onChange && onChange(CKEditor.instances[this.elementName].getData())
-          }.bind(this));
-    
-          this.setState({
-            init: true
-          })
-        }
-// -------------------------------------------------------------
-
+  componentDidMount() {
+    let newcsData
+    axios
+      .get('http://localhost:5555/reviewer/brBookcase')
+      .then(res => {
+        newcsData = res.data.rows
+        return axios.get('http://localhost:5555/reviewer/brbooks')
+      })
+      .then(res => {
+        this.setState({ csData: newcsData, bkData: res.data.rows })
+      })
+      .catch(function(error) {
+        console.log('前端沒有取得資料', error)
+      })
+  }
   render(props) {
     // if (!this.state.csData.length) return <></>
     if (this.state.csData.length === 0)
@@ -76,9 +43,12 @@ export class ReviewerBlogEdit extends React.Component {
           BlogData = csData[i]
         }
     }
+    
+    CKEDITOR.replace('editorDemo');
+    console.log(CKEDITOR.instances.editorDemo.getData());
     return (
     <>
-      <textarea ref="content1" name={this.elementName} value={this.props.content ? this.props.content : ''}></textarea>
+      <textarea name="editorDemo">範例</textarea>
 
       <h3 className="h3_br">書評家{BlogData.number} - 編輯模式</h3>
       <section className="br_CKEditor">
@@ -86,10 +56,6 @@ export class ReviewerBlogEdit extends React.Component {
             <div className="App">
             <h5 className="h5_br">你正在編輯<h3 className="h3_Red">{this.props.name}</h3></h5>
                 <CKEditor
-                id="content"
-                content={this.props.contractBodyEditor}
-                onChange={(value) => this.ckOnChange(value)}
-                
                   editor={ ClassicEditor }
                   config={{
                       toolbar: [
@@ -118,10 +84,10 @@ export class ReviewerBlogEdit extends React.Component {
                         );
                     } }
 
-                    // onChange={ ( event, editor ) => {
-                    //     const data = editor.getData();
-                    //     console.log( { event, editor, data } );
-                    // } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
 
                     onBlur={ ( event, editor ) => {
                         console.log( '失焦!Blur.', editor );
--- pbook-team/src/pages/ReviewerBlogEdit_生命週期.js
+++ pbook-team/src/pages/ReviewerBlogEdit_生命週期.js
@@ -13,52 +13,52 @@ export class ReviewerBlogEdit extends React.Component {
       init: false,
     }
   }
-  componentDidMount(){
-        let newcsData
-        axios
-          .get('http://localhost:5555/reviewer/brBookcase')
-          .then(res => {
-            newcsData = res.data.rows
-            return axios.get('http://localhost:5555/reviewer/brbooks')
-          })
-          .then(res => {
-            this.setState({ csData: newcsData, bkData: res.data.rows })
-          })
-          .catch(function(error) {
-            console.log('前端沒有取得資料', error)
-          })
-
-    // 當該組件用於創建模塊時，需要渲染editor (條件：之前從沒初始化過)
-          if (this.props.content === '' && this.props.id && this.state.init === false) {
-            this.loadCkEditor(this.props);
-          }
-        }
-    // 每次更新都會走進該週期，需要判斷有沒有初始化過 && 外部有傳進內容
-    // 當沒有init === false的判斷，會導致頁面渲染多個編輯器，
-    // 開始的解決方案不是用init作為標記，而是使用ckeditor的destory、delete等方法，發現實現起來都不太乾淨，且有問題
-        componentWillReceiveProps(nextProps) {
+  // componentDidMount(){
+  //   // 當該組件用於創建模塊時，需要渲染editor (條件：之前從沒初始化過)
+  //         if (this.props.content === '' && this.props.id && this.state.init === false) {
+  //           this.loadCkEditor(this.props);
+  //         }
+  //       }
+  //   // 每次更新都會走進該週期，需要判斷有沒有初始化過 && 外部有傳進內容
+  //   // 當沒有init === false的判斷，會導致頁面渲染多個編輯器，
+  //   // 開始的解決方案不是用init作為標記，而是使用ckeditor的destory、delete等方法，發現實現起來都不太乾淨，且有問題
+  //       componentWillReceiveProps(nextProps) {
     
-          if (CKEditor && nextProps && nextProps.content && nextProps.id  && this.state.init === false) {
-            this.loadCkEditor(nextProps);
-          }
-        }
+  //         if (CKEDITOR && nextProps && nextProps.content && nextProps.id  && this.state.init === false) {
+  //           this.loadCkEditor(nextProps);
+  //         }
+  //       }
     
-        loadCkEditor = (nextProps) => {
-          this.elementName = "editor_" + this.props.id;
+  //       loadCkEditor = (nextProps) => {
+  //         this.elementName = "editor_" + this.props.id;
     
-          CKEditor.replace(this.elementName);
-          const { onChange } = nextProps;
+  //         CKEDITOR.replace(this.elementName);
+  //         const { onChange } = nextProps;
     
-          CKEditor.instances[this.elementName].on("change", function () {
-            onChange && onChange(CKEditor.instances[this.elementName].getData())
-          }.bind(this));
+  //         CKEDITOR.instances[this.elementName].on("change", function () {
+  //           onChange && onChange(CKEDITOR.instances[this.elementName].getData())
+  //         }.bind(this));
     
-          this.setState({
-            init: true
-          })
-        }
+  //         this.setState({
+  //           init: true
+  //         })
+  //       }
 // -------------------------------------------------------------
-
+  // componentDidMount() {
+  //   let newcsData
+  //   axios
+  //     .get('http://localhost:5555/reviewer/brBookcase')
+  //     .then(res => {
+  //       newcsData = res.data.rows
+  //       return axios.get('http://localhost:5555/reviewer/brbooks')
+  //     })
+  //     .then(res => {
+  //       this.setState({ csData: newcsData, bkData: res.data.rows })
+  //     })
+  //     .catch(function(error) {
+  //       console.log('前端沒有取得資料', error)
+  //     })
+  // }
   render(props) {
     // if (!this.state.csData.length) return <></>
     if (this.state.csData.length === 0)
