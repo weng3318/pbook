40788b66302bbb394899663a916e5a6277daf8b6
weng3318 <weng7395@gmail.com>
2019-11-26 13:32:28 +0800

    1126t0132 書評列表

--- pbook-team/src/pages/ReviewerBlogEdit.js
+++ pbook-team/src/pages/ReviewerBlogEdit.js
@@ -7,10 +7,8 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 export class ReviewerBlogEdit extends React.Component {
   constructor(props) {
     super(props)
-    this.bookName = "editor_" + this.props.id; //
     this.state = {
       csData: [],
-      init: false, //
     }
   }
   componentDidMount() {
@@ -27,36 +25,6 @@ export class ReviewerBlogEdit extends React.Component {
       .catch(function(error) {
         console.log('前端沒有取得資料', error)
       })
-  // ----------------------------------------------------------------
-  // 當該組件用於創建模塊時，需要渲染editor (條件：之前從沒初始化過)
-    if (this.props.content === '' && this.props.id && this.state.init === false) {
-      this.loadCkEditor(this.props);
-    }
-      }
-      // 每次更新都會走進該週期，需要判斷有沒有初始化過 && 外部有傳進內容
-      // 當沒有init === false的判斷，會導致頁面渲染多個編輯器，
-      // 開始的解決方案不是用init作為標記，而是使用ckeditor的destory、delete等方法，發現實現起來都不太乾淨，且有問題
-      componentWillReceiveProps(nextProps) {
-
-        if (CKEditor && nextProps && nextProps.content && nextProps.id  && this.state.init === false) {
-          this.loadCkEditor(nextProps);
-        }
-      }
-
-      loadCkEditor = (nextProps) => {
-        this.bookName = "editor_" + this.props.name;
-
-        CKEditor.replace(this.bookName);
-        const { onChange } = nextProps;
-
-        CKEditor.instances[this.bookName].on("change", function () {
-          onChange && onChange(CKEditor.instances[this.bookName].getData())
-        }.bind(this));
-
-        this.setState({
-          init: true
-        })
-  // ----------------------------------------------------------------
   }
 
   render() {
@@ -76,19 +44,9 @@ export class ReviewerBlogEdit extends React.Component {
           BlogData = csData[i]
         }
     }
-    window.onload = function()
-    {
-      CKEditor.replace( 'description');
-    };
+      
     return (
     <>
-   <textarea ref="content1" name={this.bookName} value={this.props.blog ? this.props.blog : ''}></textarea>
-        {/* 書評家：<input type="text" name="position" id="position"/>
-        簡介：<input type="text" name="quantity" id="quantity"/>
-        其他職業：<input type="text" name="education" id="education"/>
-        喜好書籍：<input type="text" name="salary" id="salary"/>
-        部落格文章：<textarea name="description" id="description"></textarea> */}
-
       <h3 className="h3_br">書評家{BlogData.br_name} - 編輯模式</h3>
       <section className="br_CKEditor">
           <div className="Animate_Close_Box">
@@ -98,10 +56,10 @@ export class ReviewerBlogEdit extends React.Component {
               </div>
           </div>
 
-            <h5 className="h5_br">你正在編輯<h3 className="h3_Red">{this.props.name}</h3></h5>
+        <h5 className="h5_br">你正在編輯<h3 className="h3_Red">{this.props.name}</h3></h5>
 
         <section className="Blog_textarea">
-          <form method="post" action="/localhost:3000/reviewer/reviewerBooks">
+          <form method="post" action="">
                 <CKEditor
                   editor={ ClassicEditor }
                   config={{
@@ -119,6 +77,7 @@ export class ReviewerBlogEdit extends React.Component {
                         "redo"
                       ]
                   }}
+        
                     data={this.props.blog}
 
                     onInit={ editor => {
