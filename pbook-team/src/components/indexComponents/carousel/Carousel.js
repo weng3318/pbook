5d5876f2c647d0b71db34a7105b55be686b6aa6d
nana81210 <nana81210@hotmail.com>
2019-11-29 13:56:08 +0800

    2019/11/29 13:55nana上傳

--- pbook-team/src/components/indexComponents/carousel/Carousel.js
+++ pbook-team/src/components/indexComponents/carousel/Carousel.js
@@ -28,27 +28,27 @@ export default class Home extends React.Component {
         <Carousel.Item>
           <Link to="/activities/offline/13" target="_blank">
             <img
-              className="carousel-item d-block w-100"
+              className="carousel-item d-block"
               src="http://localhost:5555/ac/images/1200x628_20191001111150.jpg"
               alt="First slide"
             />
           </Link>
         </Carousel.Item>
         <Carousel.Item>
-          <Link to="/activities/offline/11" target="_blank">
+          <Link to="/activities/offline/16" target="_blank">
             <img
-              className="carousel-item d-block w-100"
-              src="http://localhost:5555/ac/images/1200x628_20191028180100.png"
-              alt="First slide"
+              className="carousel-item d-block"
+              src="http://localhost:5555/ac/images/CY1_20191031112600.jpg"
+              alt="Second slide"
             />
           </Link>
         </Carousel.Item>
         <Carousel.Item>
-          <Link to="/activities/offline/23" target="_blank">
+          <Link to="/activities/offline/11" target="_blank">
             <img
-              className="carousel-item d-block w-100"
-              src="http://localhost:5555/ac/images/0928_20190924180838.jpg"
-              alt="First slide"
+              className="carousel-item d-block"
+              src="http://localhost:5555/ac/images/1200x628_20191028180100.png"
+              alt="Third slide"
             />
           </Link>
         </Carousel.Item>
